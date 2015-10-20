bdd1bb92d55bd2dd75ed36fc10ebbcc5356e98eb
cassisnaro <narowirf@gmail.com>
2015-10-20 10:03:35 +0200

    Frist draft of database testing.

---
+++ tests/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'MyTestCase.php';
+
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 17/10/15
+ * Time: 21:28
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testMyTestCase()
+    {
+        $mytestcase = new MyTestCase();
+        $mytestcase->testReadDatabase();
+    }
+
+
+}
