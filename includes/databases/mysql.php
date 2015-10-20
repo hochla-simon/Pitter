d3d6975fac8b3891039399a047508676a5c75091
Christian Handschigl <admin@handschigl.com>
2015-10-20 10:14:15 +0200

    Added MySQL support.

---
+++ includes/databases/mysql.php
@@ -0,0 +1,13 @@
+<?php
+if($config['databaseHost'] != '' and $config['databaseUser'] != '' and $config['databasePassword'] != '' and $config['databaseName'] != ''){
+	$dbConnection = mysql_connect($config['databaseHost'], $config['databaseUser'], $config['databasePassword']);
+	if($dbConnection){
+		if(!mysql_select_db($config['databaseName'])){
+			createMessage('Dateabase error: Could select the database by using the specified database name.');
+		}
+	}
+	else{
+		createMessage('Dateabase error: Could not connect to database by using the specified credentials.');
+	}
+}
+?>
