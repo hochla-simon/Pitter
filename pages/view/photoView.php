f905b36f89c390f9d778e8174daf81b449ded372
ragoz4 <patrick@patoila.fi>
2015-10-23 15:31:15 +0200

    check the if the image is found in the database and remove duplicate checking for allowed file extensions

--- pages/view/photoView.php
+++ pages/view/photoView.php
@@ -3,8 +3,16 @@ $site['title'] = 'View photo';
 
 $id = $_GET['id'];
 if ($id != '') {
-    echo '<script src="' . $config['projectURL'] . '/js/scripts.js" type="text/javascript"></script>';
-    echo '<img id="picView" src="image.html?id=' . $id .'" alt=""/>';
+    $sql = "SELECT id FROM images WHERE id=" . mysql_real_escape_string($id);
+    $result = $db->query($sql);
+    $row = mysql_fetch_array($result);
+    if ($row) {
+        echo '<script src="' . $config['projectURL'] . '/js/scripts.js" type="text/javascript"></script>';
+        echo '<img id="picView" src="image.html?id=' . $id .'" alt=""/>';
+    } else {
+        include(dirname(__FILE__) . '/../common/error404.php');
+    }
+
 } else {
     include(dirname(__FILE__) . '/../common/error404.php');
 }
