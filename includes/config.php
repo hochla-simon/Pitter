ce646eb39f0c85f47c04c41036b3d17c587b402f
ragoz4 <patrick@patoila.fi>
2015-10-23 02:47:21 +0200

    Implementation of the home page

--- includes/config.php
+++ includes/config.php
@@ -24,9 +24,17 @@ if(count($tmpConfig) == 0){
 		'slogan' => 'Manage your pictures privately',
 		'copyright' => 'Copyright 2015 by <a href="https://github.com/hochla-simon/Pitter" target="_blank">Pitter</a>',
 		'homeContent' => '<h2>What is Pitter?</h2>
-			<p>This is a private photo gallery where you can store your photos securely in your own server.</p>
-			<h2>Installation</h2>
-			<p>Coming soon...</p>'
+			<p>Pitter is a private photo gallery where you can store your private photos securely in your own server.
+			It is implemented by using the most supported technologies so it is easy to install. Using the software is also very easy.
+			Uploading is done by selecting your photos from your device folder or just simply drag and drop them to the uploading area in the upload tab.
+			After the upload is finished. You can browse your photos, move them into albums and even create a slide show to watch with other people.
+			You can also share your photos with the people you trust.</p>
+			<p>Unlike other services where you can store photos, Pitter do not use your photos to other purposes.
+			In free services like Google Photos, Dropbox and Facebook, you will never know where is your photos used and what kind of information the service provider is collecting about you.
+			Pitter will not send any information or photos to anyone unless you share it with them.</p>
+			<p>Pitter is our course project in Advanced Software Engineering course in UPC (Barcelona, Spain) and it is an open source project that is maintained in Github (https://github.com/hochla-simon/Pitter).
+			We planned to do this as a open source project because we think people should have a right to store their photos privately without paying for it.
+			Also people can see from the source code, how this software will handle their photos.</p>'
 	);
 }
 
