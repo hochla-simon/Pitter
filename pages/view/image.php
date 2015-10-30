0a93f802d69ec4696551e5ba8fc5b50d29fc5a91
cassisnaro <narowirf@gmail.com>
2015-10-30 16:10:50 +0100

    thumbnails generation moved to image.php

--- pages/view/image.php
+++ pages/view/image.php
@@ -20,7 +20,46 @@ if ($extension == 'jpg') {
     $extension = "jpeg";
 }
 header('Content-Type: image/' . $extension);
-readfile($path);
+
+
+
+if(isset($_GET["max_size"])){
+    list($width, $height) = getimagesize($path);
+    $percent;
+    $max_size = $_GET["max_size"];
+    if ($width > $height){
+        $percent = $max_size/$width;
+    }else{
+        $percent = $max_size/$height;
+    }
+    $new_width = $width * $percent;
+    $new_height = $height * $percent;
+
+    // Resample
+    $image_p = imagecreatetruecolor($new_width, $new_height);
+    $image;
+    if ($extension == 'jpeg') {
+        $image = imagecreatefromjpeg($path);
+    }else {
+        if($extension == 'gif'){
+            $image = imagecreatefromgif($path);
+        }else {
+            $image = imagecreatefrompng($path);
+        }
+    }
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    if ($extension == 'jpeg') {
+        imagejpeg($image_p,null,100);
+    }else if($extension == 'gif'){
+        imagegif($image_p,null,100);
+    }else {
+        imagepng($image_p,null,100);
+    }
+    imagedestroy( $image_p );
+} else {
+    readfile($path);
+}
+
 
 die();
-?>
