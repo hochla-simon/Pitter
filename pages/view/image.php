bbac75ef48b86614c02852e9307607cac90ee90a
cassisnaro <narowirf@gmail.com>
2015-10-30 16:47:38 +0100

    thumbnails return original file if requested size is greater than the file dimension

--- pages/view/image.php
+++ pages/view/image.php
@@ -25,38 +25,48 @@ header('Content-Type: image/' . $extension);
 
 if(isset($_GET["max_size"])){
     list($width, $height) = getimagesize($path);
-    $percent;
-    $max_size = $_GET["max_size"];
+    $longest_side;
     if ($width > $height){
-        $percent = $max_size/$width;
+        $longest_side = $width;
     }else{
-        $percent = $max_size/$height;
+        $longest_side = $height;
     }
-    $new_width = $width * $percent;
-    $new_height = $height * $percent;
 
-    // Resample
-    $image_p = imagecreatetruecolor($new_width, $new_height);
-    $image;
-    if ($extension == 'jpeg') {
-        $image = imagecreatefromjpeg($path);
+    $max_size = $_GET["max_size"];
+
+    if($max_size >= $longest_side){
+        readfile($path);
     }else {
-        if($extension == 'gif'){
-            $image = imagecreatefromgif($path);
-        }else {
-            $image = imagecreatefrompng($path);
+        $percent;
+
+        $percent = $longest_side / $max_size;
+
+        $new_width = $width * $percent;
+        $new_height = $height * $percent;
+
+        // Resample
+        $image_p = imagecreatetruecolor($new_width, $new_height);
+        $image;
+        if ($extension == 'jpeg') {
+            $image = imagecreatefromjpeg($path);
+        } else {
+            if ($extension == 'gif') {
+                $image = imagecreatefromgif($path);
+            } else {
+                $image = imagecreatefrompng($path);
+            }
         }
-    }
-    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
-    if ($extension == 'jpeg') {
-        imagejpeg($image_p,null,100);
-    }else if($extension == 'gif'){
-        imagegif($image_p,null,100);
-    }else {
-        imagepng($image_p,null,100);
+        if ($extension == 'jpeg') {
+            imagejpeg($image_p, null, 100);
+        } else if ($extension == 'gif') {
+            imagegif($image_p);
+        } else {
+            imagepng($image_p);
+        }
+        imagedestroy($image_p);
     }
-    imagedestroy( $image_p );
 } else {
     readfile($path);
 }
