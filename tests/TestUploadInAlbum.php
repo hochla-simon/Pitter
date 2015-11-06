8640679618b6df88d9b82574951b6dfaab20d1a5
cassisnaro <narowirf@gmail.com>
2015-11-06 18:48:40 +0100

    Test for wrong file type.

--- tests/TestUploadInAlbum.php
+++ tests/TestUploadInAlbum.php
@@ -8,88 +8,78 @@
  */
 class TestUploadInAlbum extends PHPUnit_Extensions_Selenium2TestCase
 {
-/*public static $browsers = array(
-    array(
-        'name'    => 'Linux Firefox',
-        'browser' => '*firefox',
-        'host'    => 'localhost',
-        'port'    => 4444,
-        'timeout' => 30000,
-    ),
-    array(
-        'name'    => 'Linux Chrome',
-        'browser' => '*chrome',
-        'host'    => 'localhost',
-        'port'    => 4444,
-        'timeout' => 30000,
-    )
-);*/
+    /*public static $browsers = array(
+        array(
+            'name'    => 'Linux Firefox',
+            'browser' => '*firefox',
+            'host'    => 'localhost',
+            'port'    => 4444,
+            'timeout' => 30000,
+        ),
+        array(
+            'name'    => 'Linux Chrome',
+            'browser' => '*chrome',
+            'host'    => 'localhost',
+            'port'    => 4444,
+            'timeout' => 30000,
+        )
+    );*/
 
-protected function setUp()
-{
-    $this->setBrowser('chrome');
-    $this->setBrowserUrl('http://localhost//view/index.html?id=1/');
-}
-
-protected function waitUntilNoProgressBar()
-{
-    while(count($this->elements($this->using('css selector')->value('#myDropzone > div.dz-preview.dz-file-preview')))!=0){
-        usleep(500);
+    protected function setUp()
+    {
+        $this->setBrowser('chrome');
+        $this->setBrowserUrl('http://localhost//view/index.html?id=1/');
     }
 
-}
-
-public function testSingleFileUpload()
-{
-    /*$this->open('http://localhost//view/index.html?id=1/');
-    $this->assertTitle('Photos | Pitter');
-
-    $name = $this->byName( 'input.dz-hidden-input' );
-    echo $name;*/
-    $this->url('http://localhost//view/index.html?id=2');
-
-    //store current number of images
-    $current_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
-
-    // check the value
-    $this->assertEquals( 'image/jpeg,image/png,image/gif', $this->byCssSelector('input.dz-hidden-input')->attribute('accept'));
-
-    /*Transforming the hidden field in something that can be seen in order to be able interact with it with Selenium*/
-    $javaScriptCode = "var elemForm = $.find('input.dz-hidden-input')[0];elemForm.style.visibility='visible';elemForm.style.height=\"100px\"; elemForm.style.width=\"100px\";";
-    $this->execute(    array(
-        'script' => $javaScriptCode,
-        'args'   => array()
-    ));
-
-
-    $hiddenInput =$this->byCssSelector('input.dz-hidden-input');
-    /*Sending the file path, this trigers the same mehtod as dropping a file on the dropzone*/
-    $hiddenInput ->value('/home/daniel/Pictures/Pitufos/chevre.png');
-
-    $this->waitUntilNoProgressBar();
-
-    //getting new number of tags
-    $new_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
-    $this->assertEquals($new_number_photos,$current_number_photos+1);
-
-    //checking that there is nothing left in dropzone
-    $this->assertEquals(0,count($this->elements($this->using('css selector')->value('#myDropzone > div.dz-preview.dz-file-preview'))));
-}
-
-    public function testMultipleFileUpload()
+    protected function waitUntilNoProgressBar()
     {
-        /*$this->open('http://localhost//view/index.html?id=1/');
-        $this->assertTitle('Photos | Pitter');
+        while(count($this->elements($this->using('css selector')->value('#myDropzone > div.dz-preview.dz-file-preview')))!=0){
+            usleep(500);
+        }
 
-        $name = $this->byName( 'input.dz-hidden-input' );
-        echo $name;*/
-        $this->url('http://localhost//view/index.html?id=2');
+    }
+
+    public function testSingleFileUpload()
+    {
+        $this->url('http://localhost//view/index.html');
 
         //store current number of images
         $current_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
-        echo "current number: ".$current_number_photos;
 
         // check the value
+        $this->assertEquals( 'image/jpeg,image/png,image/gif', $this->byCssSelector('input.dz-hidden-input')->attribute('accept'));
+
+        /*Transforming the hidden field in something that can be seen in order to be able interact with it with Selenium*/
+        $javaScriptCode = "var elemForm = $.find('input.dz-hidden-input')[0];elemForm.style.visibility='visible';elemForm.style.height=\"100px\"; elemForm.style.width=\"100px\";";
+        $this->execute(    array(
+            'script' => $javaScriptCode,
+            'args'   => array()
+        ));
+
+
+        $hiddenInput =$this->byCssSelector('input.dz-hidden-input');
+        /*Sending the file path, this trigers the same mehtod as dropping a file on the dropzone*/
+        $hiddenInput ->value('/home/daniel/Pictures/Pitufos/chevre.png');
+
+        $this->waitUntilNoProgressBar();
+
+        //getting new number of tags
+        $new_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
+        $this->assertEquals($new_number_photos,$current_number_photos+1);
+
+        //checking that there is nothing left in dropzone
+        $this->assertEquals(0,count($this->elements($this->using('css selector')->value('#myDropzone > div.dz-preview.dz-file-preview'))));
+    }
+
+    public function testMultipleFileUpload()
+    {
+        $this->url('http://localhost//view/index.html');
+
+//store current number of images
+        $current_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
+        echo "current number: ".$current_number_photos;
+
+// check the value
         $this->assertEquals( 'image/jpeg,image/png,image/gif', $this->byCssSelector('input.dz-hidden-input')->attribute('accept'));
 
         /*Transforming the hidden field in something that can be seen in order to be able interact with it with Selenium*/
@@ -107,12 +97,42 @@ public function testSingleFileUpload()
 
         $this->waitUntilNoProgressBar();
 
-        //getting new number of tags
+//getting new number of tags
         $new_number_photos = count($this->elements($this->using('css selector')->value('#photos > div')));
         $this->assertEquals($current_number_photos+2, $new_number_photos);
 
-        //checking that there is nothing left in dropzone
+//checking that there is nothing left in dropzone
         $this->assertEquals(0,count($this->elements($this->using('css selector')->value('#myDropzone > div.dz-preview.dz-file-preview'))));
+    }
+
+    public function testWrongFileType()
+    {
+        $this->url('http://localhost//view/index.html');
+
+// check the value
+        $this->assertEquals( 'image/jpeg,image/png,image/gif', $this->byCssSelector('input.dz-hidden-input')->attribute('accept'));
+
+        /*Transforming the hidden field in something that can be seen in order to be able interact with it with Selenium*/
+        $javaScriptCode = "var elemForm = $.find('input.dz-hidden-input')[0];elemForm.style.visibility='visible';elemForm.style.height=\"100px\"; elemForm.style.width=\"100px\";";
+        $this->execute(    array(
+            'script' => $javaScriptCode,
+            'args'   => array()
+        ));
+
+
+
+
+        $hiddenInput =$this->byCssSelector('input.dz-hidden-input');
+        /*Sending the file path, this trigers the same mehtod as dropping a file on the dropzone*/
+        $hiddenInput ->value("/home/daniel/Documents/televisionOnDemand.odt");
+
+
+        $this->moveto($this->byCssSelector('#myDropzone > div.dz-preview.dz-file-preview.dz-error.dz-complete > div.dz-details'));
+
+        usleep(100);
+
+        $messageError = $this->byCssSelector("#myDropzone > div.dz-preview.dz-file-preview.dz-error.dz-complete > div.dz-error-message > span")->text();
+        $this->assertEquals("only jpg, jpeg, png and gif are accepted",$messageError );
     }
 }
 
