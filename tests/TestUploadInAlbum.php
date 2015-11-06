f5b0ecd7e31301d492e15cdcee111f4c1985fe5c
cassisnaro <narowirf@gmail.com>
2015-11-06 16:51:43 +0100

    First working code lines to upload through dropzo Since it is not possible to recreate a drop method, the uses the  hidden input available in dropzone.

--- tests/TestUploadInAlbum.php
+++ tests/TestUploadInAlbum.php
@@ -6,9 +6,9 @@
  * Date: 5/11/15
  * Time: 10:36
  */
-class TestUploadInAlbum extends PHPUnit_Extensions_SeleniumTestCase
+class TestUploadInAlbum extends PHPUnit_Extensions_Selenium2TestCase
 {
-public static $browsers = array(
+/*public static $browsers = array(
     array(
         'name'    => 'Linux Firefox',
         'browser' => '*firefox',
@@ -23,19 +23,40 @@ public static $browsers = array(
         'port'    => 4444,
         'timeout' => 30000,
     )
-);
+);*/
 
 protected function setUp()
 {
+    $this->setBrowser('firefox');
     $this->setBrowserUrl('http://localhost//view/index.html?id=1/');
 }
 
 public function testTitle()
 {
-    $this->open('http://localhost//view/index.html?id=1/');
+    /*$this->open('http://localhost//view/index.html?id=1/');
     $this->assertTitle('Photos | Pitter');
 
-    $name = $this->byName( 'sender_name' );
+    $name = $this->byName( 'input.dz-hidden-input' );
+    echo $name;*/
+    $this->url('http://localhost//view/index.html?id=1');
+
+    // check the value
+    $this->assertEquals( 'image/jpeg,image/png,image/gif', $this->byCssSelector('input.dz-hidden-input')->attribute('accept'));
+
+    $this->hiddenInput = $this->byCssSelector('input.dz-hidden-input');
+    //$elem=$this->byCssSelector('input.dz-hidden-input');
+
+
+    $javaScriptCode = "var elemForm = $.find('input.dz-hidden-input')[0];elemForm.style.visibility='visible';elemForm.style.height=\"100px\"; elemForm.style.width=\"100px\";";
+
+    $this->execute(    array(
+        'script' => $javaScriptCode,
+        'args'   => array()
+    ));
+    $this->hiddenInput =$this->byCssSelector('input.dz-hidden-input');
+    $this->hiddenInput ->value('/home/daniel/Pictures/Pitufos/chevre.png');
+    //$elem->value('/home/daniel/Pictures/Pitufos/chevre.png');
+    /*$this->sendKeys($elem,"/path/to/file");*/
 }
 }
 
