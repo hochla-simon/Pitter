db60b8f0fb79b73b543e50c07945d1fa1c1c67ea
cassisnaro <narowirf@gmail.com>
2015-11-19 00:29:27 +0100

    First version of the slideshow

--- pages/slideshow/index.php
+++ pages/slideshow/index.php
@@ -17,32 +17,92 @@ $sql = "SELECT id, filename, extension FROM images, imagesToAlbums WHERE images.
     <script src="<?php echo $config['projectURL'];?>js/jquery.min.js" type="text/javascript"></script>
     <?php echo $site['script'];?>
 </head>
-<body class="slideshow">
-
+<body>
+<div class="container">
 <?php
 $images = $db->query($sql);
 if (!empty($images)) {
+    echo '<div class="slideshow">';
     $count=0;
     while($row = mysql_fetch_array($images)) {
         if (file_exists(dirname(__FILE__) . '/../../data/images/' . $row['id'] . '.' . $row['extension'])) {
-            echo '<div class="slideshow"> <img class="slideshow hidden" id="'.$count.'" src="'.$config['projectURL'].'view/image.html?id=' . $row['id'] . '"/></div>';
+            echo '<img class="slideshow hidden" id="'.$count.'" src="'.$config['projectURL'].'view/image.html?id=' . $row['id'] . '"/>';
             $count = $count+1;
         }
     }
+    echo '</div>';
     if($count!=0){
-        echo '
+        ?>
         <script>
-        $( "img#0" ).load(function() {
-            $(this).removeClass("hidden");
+            var currentImage = 0;
+            var totalNumberImages = <?php echo $count?>;
+            $( "img#"+currentImage ).load(function() {
+                $(this).removeClass("hidden");
+            });
+            var tid = setInterval(transition_right, 2000);
+            function transition_right() {
+              $("img#"+currentImage).addClass("hidden");
+              currentImage = currentImage+1;
+                if(currentImage >= totalNumberImages) {
+                    currentImage=totalNumberImages-1;
+                }
+              $("img#"+currentImage).removeClass("hidden");
+            }
+            function transition_left() {
+                $("img#"+currentImage).addClass("hidden");
+                currentImage = currentImage-1;
+                if(currentImage < 0) {
+                    currentImage=0;
+                }
+                $("img#"+currentImage).removeClass("hidden");
+            }
+            function pauseTimer() { // to be called when you want to stop the timer
+              clearInterval(tid);
+              delete tid;
+            }
+            function playTimer(){
+                tid = setInterval(transition_right, 2000);
+            }
+        </script>
+        <div class="slideshow_commands">
+            <img id="slideshow_previous" class="command_icon" src="<?php echo $config['projectURL']?>images/caret-left.svg" alt="previous icon">
+            <img id="slideshow_pause" class="command_icon" src="<?php echo $config['projectURL']?>images/media-pause.svg" alt="pause icon">
+            <img id="slideshow_play" class="command_icon hidden" src="<?php echo $config['projectURL']?>images/media-play.svg" alt="play icon">
+            <img id="slideshow_next" class="command_icon" src="<?php echo $config['projectURL']?>images/caret-right.svg" alt="next icon">
+        </div>
+
+        <script>
+        $( "#slideshow_pause" ).click(function() {
+            pauseTimer();
+            $("#slideshow_play").removeClass("hidden");
+            $(this).addClass("hidden");
         });
-        </script>';
+        $( "#slideshow_play" ).click(function() {
+            playTimer();
+            $("#slideshow_pause").removeClass("hidden");
+            $(this).addClass("hidden");
+        });
+        $( "#slideshow_next" ).click(function() {
+            pauseTimer();
+            $("#slideshow_play").removeClass("hidden");
+            $('#slideshow_pause').addClass("hidden");
+            transition_right();
+        });
+        $( "#slideshow_previous" ).click(function() {
+            pauseTimer();
+            $("#slideshow_play").removeClass("hidden");
+            $('#slideshow_pause').addClass("hidden");
+            transition_left();
+        });
+        </script>
+        <?php
 
     }
 } else {
     echo '<h2>No photos!</h2>';
 }
 ?>
-
+</div>
 </body>
 </html>
 
