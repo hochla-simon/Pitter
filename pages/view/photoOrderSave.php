0863efb5ff5a1196526b643cbe84f3e25106fc6e
cassisnaro <narowirf@gmail.com>
2015-11-21 22:55:06 +0100

    Update function changed.

--- pages/view/photoOrderSave.php
+++ pages/view/photoOrderSave.php
@@ -2,6 +2,9 @@
 $parentAlbumId = $_POST["parentAlbumId"];
 foreach( $_POST['image'] as $order => $id_photo ){
     $order = $order + 1;
-    $update_sql_string = 'UPDATE imagestoalbums SET positionInAlbum="' . $order . '" WHERE imageId="' . $id_photo . '" AND albumId="'. $parentAlbumId . '" ';
+    $update_sql_string = "UPDATE `pitter`.`imagesToAlbums` SET `positionInAlbum` = '".$order."' WHERE `imagesToAlbums`.`albumId` = ".$parentAlbumId." AND `imagesToAlbums`.`imageId` =".$id_photo.";";
+    echo $update_sql_string;
     $db->query($update_sql_string);
 }
+
+die();
