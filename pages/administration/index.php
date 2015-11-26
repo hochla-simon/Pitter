1384c389494812cb3d1a1184429695245158200b
Christian Handschigl <admin@handschigl.com>
2015-11-26 11:38:12 +0100

    Partially added user administration + installation of basic administrator.

--- pages/administration/index.php
+++ pages/administration/index.php
@@ -3,5 +3,6 @@ $site['title'] = 'Administration';
 ?>
 <h2><?php echo $site['title'];?></h2>
 <ul>
+ <li><a href="<?php echo $config['projectURL']?>administration/users.html">Users</a></li>
  <li><a href="<?php echo $config['projectURL']?>administration/settings.html">Settings</a></li>
 </ul>
