bcec9861c6393f6c5426545ed398eadf1873ccc9
cassisnaro <narowirf@gmail.com>
2015-12-01 22:22:48 +0100

    dragAndDrop of Albums controls ownership.

--- pages/view/albumDragDropSave.php
+++ pages/view/albumDragDropSave.php
@@ -1,11 +1,37 @@
 <?php
-    if (isset($_POST["albumId"]) && isset($_POST["parentAlbumId"]) ) {
-        $update_sql_string = 'UPDATE albums SET parentAlbumId="' . $_POST["parentAlbumId"] . '",modified=CURRENT_TIMESTAMP() WHERE id="' . $_POST["albumId"] . '" ';
-        $db->query($update_sql_string);
-    } else {
-        http_response_code(500);
-        $db->query($delete_sql_string);
-        $message = createMessage("Sorry, there was an error moving your album.");
+    $select_sql_string = "SELECT id, parentAlbumId, name, ownerId, description FROM albums WHERE id=" . mysql_real_escape_string($_POST["albumId"]);
+    $result = $db->query($select_sql_string);
+    if (!empty($result)){
+        $album = mysql_fetch_array($result);
+        if ($album['ownerId'] != $currentUser['id']) {
+            $denied = true;
+        }
+    }else{
+        $denied = true;
     }
-    die();
+    $select_sql_string = "SELECT id, parentAlbumId, name, ownerId, description FROM albums WHERE id=" . mysql_real_escape_string($_POST["parentAlbumId"]);
+    $result = $db->query($select_sql_string);
+    if (!empty($result)){
+        $album = mysql_fetch_array($result);
+        if ($album['ownerId'] != $currentUser['id']) {
+            $denied = true;
+        }
+    }else{
+        $denied = true;
+    }
+
+    if($denied){
+        include(dirname(__FILE__) . '/../common/error401.php');
+        exit();
+    }else {
+        if (isset($_POST["albumId"]) && isset($_POST["parentAlbumId"])) {
+            $update_sql_string = 'UPDATE albums SET parentAlbumId="' . $_POST["parentAlbumId"] . '",modified=CURRENT_TIMESTAMP() WHERE id="' . $_POST["albumId"] . '" ';
+            $db->query($update_sql_string);
+        } else {
+            http_response_code(500);
+            $db->query($delete_sql_string);
+            $message = createMessage("Sorry, there was an error moving your album.");
+        }
+        die();
+   }
 ?>
--- pages/view/photoMove.php
+++ pages/view/photoMove.php
@@ -4,6 +4,42 @@ $imageId = $_POST["imageId"];
 $albumId = $_POST["albumId"];
 $newAlbumId = $_POST["newAlbumId"];
 
+$select_sql_string = "SELECT id, parentAlbumId, name, ownerId, description FROM albums WHERE id=" . mysql_real_escape_string($albumId);
+$result = $db->query($select_sql_string);
+if (!empty($result)){
+    $album = mysql_fetch_array($result);
+    if ($album['ownerId'] != $currentUser['id']) {
+        $denied = true;
+    }
+}else{
+    $denied = true;
+}
+$select_sql_string = "SELECT id, parentAlbumId, name, ownerId, description FROM albums WHERE id=" . mysql_real_escape_string($newAlbumId);
+$result = $db->query($select_sql_string);
+if (!empty($result)){
+    $album = mysql_fetch_array($result);
+    if ($album['ownerId'] != $currentUser['id']) {
+        $denied = true;
+    }
+}else{
+    $denied = true;
+}
+$sql = "SELECT id,ownerId FROM images WHERE id=" . mysql_real_escape_string($id);
+$result = $db->query($sql);
+$row = mysql_fetch_array($result);
+if (!empty($row)) {
+    if($row['ownerId']==$currentUser['id']) {
+        $denied = true;
+    }
+}else{
+    $denied = true;
+}
+
+if($denied){
+    include(dirname(__FILE__) . '/../common/error401.php');
+    exit();
+}
+
 $sql = "SELECT positionInAlbum FROM imagesToAlbums WHERE albumId = " . mysql_real_escape_string($newAlbumId) . " ORDER BY positionInAlbum DESC LIMIT 1";
 $currentMaxPosition = $db->query($sql);
 $newMaxPosition = (int) mysql_fetch_array($currentMaxPosition)["positionInAlbum"] + 1;
