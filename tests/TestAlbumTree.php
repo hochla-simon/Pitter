37172518e88dccb4b32b72b444ebad8b067764f0
ragoz4 <patrick@patoila.fi>
2015-12-02 21:37:59 +0100

    login for selenium tests

---
+++ tests/TestAlbumTree.php
@@ -0,0 +1,29 @@
+<?php
+class TestAlbumTree extends PHPUnit_Extensions_Selenium2TestCase {
+    public $email = ''; //Admin email here
+    public $password = ''; //Admin password here
+    public $projectURL;
+
+    protected function setUp() {
+        $this->setBrowser('chrome');
+        $readedConfig = json_decode(@file_get_contents(dirname(__FILE__).'/data/confForTests.txt'), true);
+        $this->projectURL=$readedConfig['projectURL'];
+        $this->setBrowserUrl($this->projectURL.'/view/index.html');
+    }
+
+    protected function login() {
+        $this->byId('setting_email')->value($this->email);
+        $this->byId('setting_password')->value($this->password);
+        $this->byClassName('submit')->click();
+        echo $this->email;
+        echo $this->password;
+    }
+
+    public function testAlbums() {
+        $this->url($this->projectURL.'/users/login.html');
+        $this->login();
+        $this->url($this->projectURL.'/view/index.html');
+        sleep(5);
+    }
+}
+?>
