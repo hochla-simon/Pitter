1e4662aa619d472e105728560eb062d871af75da
cegimberna <celine.gimbernat@gmail.com>
2015-12-03 11:34:49 +0100

    tests delete photo in multiple albums

--- tests/testImage.php
+++ tests/testImage.php
@@ -35,7 +35,7 @@ class testImage extends PHPUnit_Framework_TestCase {
 
         include(dirname(__FILE__).'/../index.php');
 
-        //Test delete photo
+        //Test delete photo from one album
         $db->query('INSERT INTO images (ownerId, name, filename, extension, created, description) VALUES ("1", "", "flamingo","jpg", CURRENT_TIMESTAMP(),"")');
         $newImageId = mysql_insert_id();
         $db->query('INSERT INTO imagestoalbums (albumId, imageId, positionInAlbum) VALUES ("1", "'. $newImageId .'", "1")');
@@ -54,6 +54,43 @@ class testImage extends PHPUnit_Framework_TestCase {
         include(dirname(__FILE__).'/../pages/view/photoDelete.php');
         $results = mysql_num_rows($db->query('SELECT * FROM images'));
         $this->assertEquals('1',$results);
+
+        //Test delete photo from one album but existing in two
+        $db->query('INSERT INTO images (ownerId, name, filename, extension, created, description) VALUES ("1", "", "flamingo","jpg", CURRENT_TIMESTAMP(),"")');
+        $newImageId = mysql_insert_id();
+        $db->query('INSERT INTO imagestoalbums (albumId, imageId, positionInAlbum) VALUES ("1", "'. $newImageId .'", "1")');
+        $db->query('INSERT INTO imagestoalbums (albumId, imageId, positionInAlbum) VALUES ("2", "'. $newImageId .'", "1")');
+
+        $target = dirname(__FILE__).'/data/uploadTest/flamingos.jpg'; // Ceci est le fichier qui existe actuellement
+        $link = dirname(__FILE__).'/../data/images/'.$newImageId.'.jpg';  // Ceci sera le nom du fichier que vous voulez lier
+        link($target, $link);
+        $_GET = array(
+            'id' => $newImageId
+        );
+        $_POST = array(
+            'Delete' => true,
+            'album' => array(1)
+        );
+
+        include(dirname(__FILE__).'/../pages/view/photoDelete.php');
+        $results = mysql_num_rows($db->query('SELECT * FROM images'));
+        $this->assertEquals('2',$results);
+
+        //Test delete photo from two album, photo in only two albums
+        $db->query('INSERT INTO imagestoalbums (albumId, imageId, positionInAlbum) VALUES ("1", "'. $newImageId .'", "1")');
+
+        $_GET = array(
+            'id' => $newImageId
+        );
+        $_POST = array(
+            'Delete' => true,
+            'album' => array(1, 2)
+        );
+
+        include(dirname(__FILE__).'/../pages/view/photoDelete.php');
+        $results = mysql_num_rows($db->query('SELECT * FROM images'));
+        $this->assertEquals('1',$results);
+
     }
 
 }
