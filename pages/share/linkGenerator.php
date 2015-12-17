5c5a6f2fc4163c4a24a668ac6e5157d841b22519
cassisnaro <narowirf@gmail.com>
2015-12-17 11:45:07 +0100

    php page to create sharelinks added.

---
+++ pages/share/linkGenerator.php
@@ -0,0 +1,38 @@
+<?php
+$albumId = $_GET['id'];
+if (!$albumId) {
+    include(dirname(__FILE__) . '/../common/error401.php');
+    exit();
+}
+$query_for_album = "SELECT parentAlbumId, id, ownerId, name FROM albums WHERE id='" . mysql_real_escape_string($albumId) . "'";
+$album_data = mysql_fetch_array($db->query($query_for_album));
+if (!empty($album_data)) {
+    if ($album_data['ownerId'] != $currentUser['id']) {
+        include(dirname(__FILE__) . '/../common/error401.php');
+    }else{
+        $sql = "SELECT * FROM `linkToAlbums` WHERE `albumId` = ".$albumId.";";
+        $result = $db->query($sql);
+        $row = mysql_fetch_array($result);
+        if(empty($row)){
+            $link="";
+            $iter=0;
+            $linkValid = false;
+            do{
+                $link=base64_encode(crypt($albumId+$iter, strval(rand(0, 1048576))));
+                $sql = "SELECT * FROM `linkToAlbums` WHERE  `link` = '".mysql_real_escape_string($link)."'";
+                $result = $db->query($sql);
+                $row = mysql_fetch_array($result);
+                if(empty($row)) {
+                    $linkValid = true;
+                }
+
+            }while(!$linkValid);
+            $sqlInsertQuery = 'INSERT INTO linkToAlbums (albumId,link) VALUES (' . $albumId . ',"' . mysql_real_escape_string($link) . '");';
+            $db->query($sqlInsertQuery);
+            echo $link;
+        }else{
+            echo $row['link'];
+        }
+    }
+}
+die();
