81387611b7ce377c7552b2c1a4bc7eff0531fc3d
ragoz4 <patrick@patoila.fi>
2015-12-28 19:49:21 +0100

    Show metadata in photo view

--- pages/view/photoView.php
+++ pages/view/photoView.php
@@ -1,4 +1,18 @@
 <?php
+
+function addMetadata($id, $db) {
+    $select_sql_query = "SELECT name, value FROM metadata WHERE imageId = " . mysql_real_escape_string($id);
+    $result = $db->query($select_sql_query);
+    if ($result != false) {
+        while ($row = mysql_fetch_array($result)) {
+            echo '<tr>';
+            echo '<td>' . $row['name'] . ':</td>';
+            echo '<td>' . $row['value'] . '</td>';
+            echo '</tr>';
+        }
+    }
+}
+
 if($currentUser['id'] == ''):
     $_POST['redirect'] = $_SERVER['REQUEST_URI'];
     include(dirname(__FILE__).'/../users/login.php');
@@ -7,7 +21,7 @@ $site['title'] = 'View photo';
 
 $id = $_GET['id'];
 if ($id != '') {
-    $sql = "SELECT id,ownerId,name FROM images WHERE id=" . mysql_real_escape_string($id);
+    $sql = "SELECT id,ownerId,name,filename,extension FROM images WHERE id=" . mysql_real_escape_string($id);
     $result = $db->query($sql);
     $row = mysql_fetch_array($result);
 
@@ -24,6 +38,19 @@ if ($id != '') {
             echo '<h2 id="photo_name">' . $row['name'] . '</h2>';
             echo '<script src="' . $config['projectURL'] . '/js/photoViewScripts.js" type="text/javascript"></script>';
             echo '<img id="picView" src="image.html?id=' . $id . '" alt=""/>';
+
+            echo '<div>';
+            echo '<table id="metadata">';
+            echo '<tr>';
+            echo '<th colspan="2">Metadata</th>';
+            echo '</tr>';
+            echo '<tr>';
+            echo '<td>Original file name:</td>';
+            echo '<td>' . $row['filename'] . '.' . $row['extension'] . '</td>';
+            echo '</tr>';
+            addMetadata($id, $db);
+            echo '</table>';
+            echo '</div>';
         }else{
             include(dirname(__FILE__) . '/../common/error401.php');
         }
